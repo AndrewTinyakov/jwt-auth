eaab289842b3a3f68b2825b7752baa6d262b3776
AndrewTinyakov <oker.2005@mail.ru>
2024-02-08 19:41:36 +0300

    added taken username and email handlers

--- src/main/java/com/example/jwtauth/security/exception/handler/SecurityExceptionHandler.java
+++ src/main/java/com/example/jwtauth/security/exception/handler/SecurityExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.jwtauth.security.exception.handler;
 
 import com.example.jwtauth.security.exception.ForbiddenException;
+import com.example.jwtauth.security.exception.registration.TakenEmailException;
+import com.example.jwtauth.security.exception.registration.TakenUsernameAndEmailException;
+import com.example.jwtauth.security.exception.registration.TakenUsernameException;
 import com.example.jwtauth.security.token.refresh.exception.ExpiredRefreshTokenException;
 import com.example.jwtauth.security.token.refresh.exception.InvalidRefreshTokenException;
 import lombok.extern.slf4j.Slf4j;
@@ -46,6 +49,42 @@ public class SecurityExceptionHandler {
 
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("error", "Refresh token is invalid");
+        body.put("timestamp", LocalDateTime.now());
+
+        return new ResponseEntity<>(body, OK);
+    }
+
+    @ExceptionHandler(TakenUsernameAndEmailException.class)
+    public ResponseEntity<?> handleTakenUsernameAndEmailException() {
+        log.warn("TakenUsernameAndEmailException");
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("error", "Username and email are taken");
+        body.put("type", "both");
+        body.put("timestamp", LocalDateTime.now());
+
+        return new ResponseEntity<>(body, OK);
+    }
+
+    @ExceptionHandler(TakenEmailException.class)
+    public ResponseEntity<?> handleTakenEmailException() {
+        log.warn("TakenEmailException");
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("error", "Email is taken");
+        body.put("type", "email");
+        body.put("timestamp", LocalDateTime.now());
+
+        return new ResponseEntity<>(body, OK);
+    }
+
+    @ExceptionHandler(TakenUsernameException.class)
+    public ResponseEntity<?> handleTakenUsernameException() {
+        log.warn("TakenUsernameException");
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("error", "Username is taken");
+        body.put("type", "username");
         body.put("timestamp", LocalDateTime.now());
 
         return new ResponseEntity<>(body, OK);
